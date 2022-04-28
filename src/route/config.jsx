70564503b7feddbb5a8008bb7e79d8c5019efa70
张伟 <zhangwei@MacBook-Air.local>
2022-04-28 09:34:41 +0800

    添加ant的样式

--- src/route/config.jsx
+++ src/route/config.jsx
@@ -1,26 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2022-04-24 13:58:50
- * @LastEditTime: 2022-04-25 13:16:59
+ * @LastEditTime: 2022-04-28 09:33:13
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /mac/vite/vite-dmeo/src/route/config.js
  */
-import Home from "page/home/index"
-import codeLab from "page/codeLab/index"
-import NotFound from "page/notFonund/index"
+import React from 'react';
+
+//优化文件体积
+let Home = React.lazy(()=>import("page/home/index"))
+
+let NotFound = React.lazy(() => import("page/notFonund/index"))
+
+
+
+
 const Configs = [
     {
         path: '/',
         exact: true,
         component: Home,
-        role: 'user',       // 当前路由需要的角色权限
-        backUrl: '/login'   // 不满足权限跳转的路由
-    },
-    {
-        path: '/codeLab',
-        exact: true,
-        component: codeLab,
         role: 'user',       // 当前路由需要的角色权限
         backUrl: '/login'   // 不满足权限跳转的路由
     },
